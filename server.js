f5c3d14dbd1e21206465caf7380b187a83702671
Ishfaq Ahmad Dar <ishfaqahmad@Ishfaqs-MacBook-Pro.local>
2024-12-17 19:52:18 +0530

    commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ function broadcastMessage(message) {
   );
 }
 
-const PORT = 5000;
+const PORT = 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
